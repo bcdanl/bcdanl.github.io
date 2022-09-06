a03c2c9b24088cc916f05d93706d7a0166622fea
bcdanl <bchoe@geneseo.edu>
2022-09-05 20:20:21 -0400

    lec3

---
+++ render_site_multiple.R
@@ -0,0 +1,2 @@
+rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/index.Rmd")
+rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/resume.Rmd")
