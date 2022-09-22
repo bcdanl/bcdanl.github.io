8ec19f5144a842c937710052b608723659fb9110
bcdanl <bchoe@geneseo.edu>
2022-09-21 23:45:34 -0400

    lec8

--- render_site_multiple.R
+++ render_site_multiple.R
@@ -1,4 +1,4 @@
-rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/index.Rmd")
-rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/resume.Rmd")
-rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/DANL100_hw1q.Rmd")
-rmarkdown::render_site("/Users/byeong-hakchoe/Documents/website/bcdanl.github.io/DANL200_hw1q.Rmd")
+rmarkdown::render_site("index.Rmd")
+rmarkdown::render_site("resume.Rmd")
+rmarkdown::render_site("DANL100_hw1q.Rmd")
+rmarkdown::render_site("DANL200_hw1q.Rmd")
