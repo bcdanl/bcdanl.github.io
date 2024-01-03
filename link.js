55e6327fde4337b491ba6b88e509903f649728bc
bcdanl <bchoe@geneseo.edu>
2024-01-03 16:52:19 -0700

    comments

--- link.js
+++ link.js
@@ -1,11 +1,5 @@
 window.onload = function() {
-    document.querySelectorAll('a').forEach(function(link) {
+    document.querySelectorAll('td a').forEach(function(link) {
         link.setAttribute('target', '_blank');
-    });
-};
-
-window.onload = function() {
-    document.querySelectorAll('.quarto-header a').forEach(function(link) {
-        link.setAttribute('target', '_self');
     });
 };
