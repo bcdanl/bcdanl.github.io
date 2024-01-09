84f36ed7f2757f5bedee13b7b82dda164adad4af
bcdanl <bchoe@geneseo.edu>
2024-01-08 22:14:40 -0700

    index

--- index-target-blank.R
+++ index-target-blank.R
@@ -2,6 +2,9 @@
 lec_no <- '01'
 lec_date <- '0123'
 
+old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
+new_content <- str_c(old_content, " ", 'target="_blank"') 
+
 quarto::quarto_render("index.qmd")
 
 library(stringr)
@@ -10,13 +13,11 @@ html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
-# Find the element to replace 
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
 html_as_text <- str_replace_all(html_as_text,
                                 old_content,
                                 new_content)
+
+
 writeLines(html_as_text, 
            "index.html")
 
