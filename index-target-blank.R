f377c6e78f6955282b2c82acbcfab60b5c887ce7
bcdanl <bchoe@geneseo.edu>
2024-08-28 03:33:21 -0400

    fig

--- index-target-blank.R
+++ index-target-blank.R
@@ -8,23 +8,32 @@ html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
+# Next lecture number & dates
+lec_dates <- c('0826', '0828')
+lec_n <- length(lec_dates)
 
+lec_nos <- 1:lec_n
+lec_nos <- sprintf("%02d", lec_nos)
+
+for (i in 1:length(lec_nos)){
+  lec_no <- lec_nos[i]
+  lec_date <- lec_dates[i]
   
-old_content <- "<title> | Byeong-Hak Choe</title>"
-new_content <- "<title>B.H. Choe</title>"
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
+  old_content <- str_c('./danl-lec/danl-101-lec-', lec_no, '-2024-', lec_date, '.html')
+  new_content <- str_c('#', 
+                       '" onclick="', 
+                       "window.open(", 
+                       "'", 
+                       old_content, 
+                       "'); return false;", '"') 
+  
+  html_as_text <- str_replace_all(html_as_text,
+                                  old_content,
+                                  new_content)
+}
 
 writeLines(html_as_text, 
            "index.html")
 
-
-
-# file.copy("tmp/index.html", "index.html")
-
-# Use system() to execute the rm command
-# system(paste("rm", shQuote("index.html")))
-# Sys.sleep(2)
-# 
-# file.rename("index2.html", "index.html")
+# <a href="./econ-lec/econ-341-lec-01-2024-0826.html" class="title listing-title">Lecture 1</a>
+# <a href="#" onclick="window.open('./econ-lec/econ-341-lec-01-2024-0826.html'); return false;" class="title listing-title">Lecture 1</a>
